25a65039bd83382b4b387f601f98884eba998e25
Saifur Rahman <100674849+saifu-r@users.noreply.github.com>
2024-02-12 20:20:44 +0600

    Time: 59 ms (25.81%), Space: 48.8 MB (27.95%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+//     ------------binary search-----------------
+//     var low= 0
+//     var high= nums.length-1
+//     while(low<= high){
+//         var mid= low + (high-low)/2
+        
+//         if(nums[mid] == target){
+//             return mid
+//         }else if(nums[mid]< target){
+//             low= mid + 1
+//         }else{
+//             high= mid-1
+//         }
+//     }
+//     return low
+//     
+    
+//     -----------for loop [O(n)]-----------
+    
+    for(var i = 0; i< nums.length; i++){
+        if(nums[i]>= target){
+            return i
+        }
+    }
+    return nums.length
+     
+};
