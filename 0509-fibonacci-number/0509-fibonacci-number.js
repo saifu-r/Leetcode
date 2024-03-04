4f943bc40cafbc754460551c91ac2ea82230924b
Saifur Rahman <100674849+saifu-r@users.noreply.github.com>
2024-03-04 16:44:14 +0600

    Time: 66 ms (45.81%), Space: 48.2 MB (64.57%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -4,14 +4,9 @@
  */
 var fib = function(n) {
     
-    function fibonacci(n){
-        if(n==0) return 0
-        else if(n==1) return 1
-        
-        else{
-            return fibonacci(n-1)+ fibonacci(n-2)
-        }
-        
+    if(n<=1) return n
+    
+    else{
+        return fib(n-1)+ fib(n-2)
     }
-    return fibonacci(n)
 };
