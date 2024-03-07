5afb84eb16099a9c6565786850f16e840105b5bc
Saifur Rahman <100674849+saifu-r@users.noreply.github.com>
2024-03-07 10:05:24 +0600

    Time: 52 ms (52.27%), Space: 49 MB (28.97%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    words= []
+    s= s.trim()
+    
+    if(s===null|| s.length===0) return 0
+    
+    words= s.split(' ')    
+    var newWord= words[words.length-1]
+    
+    return newWord.length
+    
+    
+};
